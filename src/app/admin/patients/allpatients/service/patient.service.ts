b05898c7d2fff9ba4b72b944b4d1ef72703d104e
Jocselyn Aguilar <ajocseline@gmail.com>
2023-11-05 17:17:31 -0600

    bloqueo de pacientes listos

---
+++ src/app/admin/patients/allpatients/service/patient.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import { Patient } from '../models/patient.model';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {API_URL} from "../../../../../config";
+import {AuthService} from "@core";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PatientService {
+  private readonly baseUrl = API_URL;
+
+  constructor(private httpClient: HttpClient, private readonly authService : AuthService) {}
+
+
+  getAllPatients(): Observable<Patient[]> {
+    const url = `${this.baseUrl}/patients-system`
+    return this.httpClient.get<Patient[]>(url);
+  }
+
+  changeState(id: number){
+    const header = new HttpHeaders().set("Authorization", 'Bearer ' + this.authService.currentUserValue.token)
+    const url = `${this.baseUrl}/changeStatusUser/${id}`
+    return this.httpClient.put(url, {}, {headers: header});
+  }
+
+}

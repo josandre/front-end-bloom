e6fc0f8895af0b88d88160c468e2045d267339f1
fbnmlnss <fabimilanes@gmail.com>
2023-11-06 15:19:22 -0600

    display doctors in admin view

--- src/app/admin/doctors/doctors.module.ts
+++ src/app/admin/doctors/doctors.module.ts
@@ -3,20 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DoctorsRoutingModule } from './doctors-routing.module';
 import { AlldoctorsComponent } from './alldoctors/alldoctors.component';
-import { DeleteDialogComponent } from './alldoctors/dialogs/delete/delete.component';
-import { FormDialogComponent } from './alldoctors/dialogs/form-dialog/form-dialog.component';
 import { AddDoctorComponent } from './add-doctor/add-doctor.component';
 import { EditDoctorComponent } from './edit-doctor/edit-doctor.component';
 import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
-import { DoctorsService } from './alldoctors/doctors.service';
 import { ComponentsModule } from '@shared/components/components.module';
 import { SharedModule } from '@shared';
 
 @NgModule({
   declarations: [
     AlldoctorsComponent,
-    DeleteDialogComponent,
-    FormDialogComponent,
     AddDoctorComponent,
     EditDoctorComponent,
     DoctorProfileComponent,
@@ -29,6 +24,6 @@ import { SharedModule } from '@shared';
     ComponentsModule,
     SharedModule,
   ],
-  providers: [DoctorsService],
+  providers: [],
 })
 export class DoctorsModule {}
---
+++ src/app/admin/doctors/service/doctor.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+
+import { Doctor } from '../model/Doctor';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { API_URL } from 'config';
+import {AuthService} from "@core";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DoctorService {
+  private readonly baseUrl = API_URL;
+
+  constructor(
+    private httpClient: HttpClient,
+    private readonly authService : AuthService) {}
+
+
+  getAllDoctors(): Observable<Doctor[]> {
+    const url = `${this.baseUrl}/specialists`;
+    
+    return this.httpClient.get<Doctor[]>(url);
+  }
+
+  changeState(id: number){
+    const header = new HttpHeaders().set("Authorization", 'Bearer ' + this.authService.currentUserValue.token)
+
+    const url = `${this.baseUrl}/changeStatusUser/${id}`;
+
+    return this.httpClient.put(url, {}, {headers: header});
+  }
+}

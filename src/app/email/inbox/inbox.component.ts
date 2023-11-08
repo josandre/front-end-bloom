405ecb7a9437ee144c100ca38d852cd506bf6716
esequeiras <esequeiras@ucenfotec.ac.cr>
2023-11-08 17:53:40 -0600

    get check id to delete

--- src/app/email/inbox/inbox.component.ts
+++ src/app/email/inbox/inbox.component.ts
@@ -8,10 +8,14 @@ import {Resource} from "../models/Resource";
 })
 export class InboxComponent implements OnInit{
 
-  constructor(private readonly resourceService: ResourceService) {  }
+  constructor(private readonly resourceService: ResourceService) {
+    this.selectedResourceIds=[];
+    }
 
   resourcesList: Resource[];
   originalResourcesList: Resource[];
+  selectedResourceIds: number[];
+
   ngOnInit(){
     this.resourceService.getResourceList().subscribe(
       resources =>{ 
@@ -25,16 +29,34 @@ export class InboxComponent implements OnInit{
   }
   applyFilter(filterValue: any) {
     let filterText: string = filterValue.value;
-  filterText = filterText.trim();
-  filterText = filterText.toLowerCase();
+    filterText = filterText.trim();
+    filterText = filterText.toLowerCase();
 
-  if (filterText) {
-    this.resourcesList = this.originalResourcesList.filter((resource) =>
-      resource.title.toLowerCase().includes(filterText)
-    );
-  } else {
-    this.resourcesList = [...this.originalResourcesList]; // Restaurar la lista original
+    if (filterText) {
+      this.resourcesList = this.originalResourcesList.filter((resource) =>
+        resource.title.toLowerCase().includes(filterText)
+      );
+    } else {
+      this.resourcesList = [...this.originalResourcesList]; // Restaurar la lista original
+    }
   }
-   }
+  readResourceCheck(id: number) {
+    console.log(id);
+    const index = this.selectedResourceIds.indexOf(id);
+  
+    if (index === -1) {
+      // Si no se encontró el ID en el arreglo, lo añade
+      this.selectedResourceIds.push(id);
+      console.log("No estaba seleccionado pero ahora si",id);
+    } else {
+      // deselecciona
+      this.selectedResourceIds.splice(index, 1);
+      console.log("ya no estoy seleccionado",id);
+
+    }
+  }
+  deleteResourse(){
+    
+  }
 
 }

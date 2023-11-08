5974c9de2af38ad941f18155664994b462d04dee
esequeiras <esequeiras@ucenfotec.ac.cr>
2023-11-08 15:21:09 -0600

    filter resource doctor

--- src/app/email/inbox/inbox.component.ts
+++ src/app/email/inbox/inbox.component.ts
@@ -11,15 +11,30 @@ export class InboxComponent implements OnInit{
   constructor(private readonly resourceService: ResourceService) {  }
 
   resourcesList: Resource[];
-
+  originalResourcesList: Resource[];
   ngOnInit(){
     this.resourceService.getResourceList().subscribe(
-      resources =>{ this.resourcesList = resources;}
+      resources =>{ 
+        this.resourcesList = resources;
+        this.originalResourcesList=[...resources]}
     )
   }
 
   readResource(id: number){
     sessionStorage.setItem('resourseId', id.toString());
   }
+  applyFilter(filterValue: any) {
+    let filterText: string = filterValue.value;
+  filterText = filterText.trim();
+  filterText = filterText.toLowerCase();
+
+  if (filterText) {
+    this.resourcesList = this.originalResourcesList.filter((resource) =>
+      resource.title.toLowerCase().includes(filterText)
+    );
+  } else {
+    this.resourcesList = [...this.originalResourcesList]; // Restaurar la lista original
+  }
+   }
 
 }

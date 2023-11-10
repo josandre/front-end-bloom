f261e7b251bb0b739bf46a67c9351152cacf4b0d
Jocselyn Aguilar <ajocseline@gmail.com>
2023-11-09 18:36:29 -0600

    session connection

--- src/app/apps/chat/chat.component.ts
+++ src/app/apps/chat/chat.component.ts
@@ -1,32 +1,62 @@
-import {Component, OnInit} from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 import {Conversation} from "./models/Conversation";
 import {ConversationService} from "./services/conversation.service";
+import {WebSocketService} from "./services/web-socket.service";
+import {Message} from "./models/Message";
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent implements OnInit{
+export class ChatComponent implements OnInit, OnDestroy{
   hideRequiredControl = new FormControl(false);
   conversations : Array<Conversation>
   isLoading : boolean = true;
+  authForm!: FormGroup;
 
-  constructor(private readonly conversationService: ConversationService) {}
+  constructor(private formBuilder: FormBuilder,private readonly conversationService: ConversationService, public webSocketService: WebSocketService) {}
 
 
   ngOnInit(): void {
+    this.authForm = this.formBuilder.group({
+      message: new FormControl("", {
+        validators: [Validators.required],
+        updateOn: "submit"
+      }),
+    })
+
     this.loadConversations()
+    this.webSocketService.openWebSocket();
+
   }
 
   private loadConversations(){
     this.conversationService.getConversationsByUserId().subscribe((conversationResponse) => {
       this.conversations = conversationResponse.conversationList;
-      console.log(this.conversations)
       this.isLoading = false;
     })
   }
 
+  sendMessage(){
+    console.log('click')
+    console.log(this.authForm.valid)
+    if(this.authForm.valid){
+      const message = new Message({
+        message: this.authForm.controls['message'].value,
+        receiverId: 26 // change to selected chat
+      })
+
+      console.log(message)
+      this.webSocketService.sendMessage(message);
+    }
+
+
+  }
+
+  ngOnDestroy() {
+    this.webSocketService.closeWebSocket()
+  }
 
 
 

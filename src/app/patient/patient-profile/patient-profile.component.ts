8b5e292786e42bf307ef6494c6272c95fd2ada86
esequeiras <esequeiras@ucenfotec.ac.cr>
2023-11-12 18:53:34 -0600

    add patient profile

---
+++ src/app/patient/patient-profile/patient-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-patient-profile',
+  templateUrl: './doctor-profile.component.html',
+  styleUrls: ['./doctor-profile.component.scss'],
+})
+export class PatientProfileComponent {
+  constructor() {
+    // constructor code
+  }
+}

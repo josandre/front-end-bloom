6675d5c0413d806f11b0ecb018aa92bf5e53beeb
esequeiras <esequeiras@ucenfotec.ac.cr>
2023-11-21 19:14:39 -0600

    contacts

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -8,6 +8,7 @@ import { DeleteComponent } from './delete/delete.component';
 import { ContactsService } from './contacts.service';
 import { ComponentsModule } from '@shared/components/components.module';
 import { SharedModule } from '@shared';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ContactsComponent, contactForm, DeleteComponent],
@@ -18,6 +19,7 @@ import { SharedModule } from '@shared';
     AdvanceTableRoutingModule,
     ComponentsModule,
     SharedModule,
+    TranslateModule
   ],
   providers: [ContactsService],
 })

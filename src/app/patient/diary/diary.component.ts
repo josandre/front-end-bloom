e1b6b5fd2c9e54d70655a9f7d3d1c26bc675aca4
fbnmlnss <fabimilanes@gmail.com>
2023-11-23 04:28:15 -0600

    feat: creates entries

--- src/app/patient/diary/diary.component.ts
+++ src/app/patient/diary/diary.component.ts
@@ -7,83 +7,107 @@ import {Diary} from "./model/diary";
 import {Entry} from "./model/entry";
 
 @Component({
-  selector: 'app-diary',
-  templateUrl: './diary.component.html',
-  styleUrls: ['./diary.component.scss'],
+    selector: 'app-diary',
+    templateUrl: './diary.component.html',
+    styleUrls: ['./diary.component.scss'],
 })
 
 export class DiaryComponent implements OnInit {
-  @ViewChild('editor')
-  public Editor: any;
-  editorContent: string;
-  editorHidden: boolean;
+    @ViewChild('editor')
+    public Editor: any;
+    editorContent: string;
+    editorHidden: boolean;
 
-  diary: Diary;
-  diaryId: number;
-  entries?: Entry[];
-  entryId: number;
+    diary: Diary;
+    diaryId: number;
 
-  loading: boolean;
+    entries?: Entry[];
+    currentEntryId?: number;
+    entryCanBeSaved: boolean;
 
-  constructor(
-    public diaryService: DiaryService) {
-    this.Editor = ClassicEditor;
-    this.editorHidden = true;
-  }
+    loading: boolean;
 
-  ngOnInit(): void {
-    this.loading = true;
-    this.getDiary();
-  }
+    constructor(
+        public diaryService: DiaryService) {
+        this.Editor = ClassicEditor;
+        this.editorHidden = true;
+    }
 
-  getDiary(): void {
-    this.diaryService.getDiaryByUser()
-      .subscribe(
-        (data) => {
-          this.diary = data;
-          this.diaryId = this.diary.id;
-          this.entries = this.diary.entries;
+    ngOnInit(): void {
+        this.loading = true;
+        this.getDiary();
+    }
 
-          this.loading = false;
-        },
-        error => {
-          console.log(error.status);
+    getDiary(): void {
+        this.diaryService.getDiaryByUser()
+            .subscribe(
+                (data) => {
+                    this.diary = data;
+                    this.diaryId = this.diary.id;
+                    this.entries = this.diary.entries;
+
+                    this.loading = false;
+                },
+                error => {
+                    console.log(error.status);
+                });
+    }
+
+    addEntry() {
+        this.entryCanBeSaved = false;
+        this.editorHidden = false;
+
+        const entry: Entry = new Entry({
+            content: ""
         });
-  }
 
-  addEntry() {
-    this.diaryService.createEntry(this.diaryId)
-      .subscribe(
-        (response) => {
-          console.log(response);
-        },
-        error => {
-          switch (error.status) {
-            case -1: {
-              //this.openSnackBar("Something went wrong while trying to register anxiety type", "Try again");
-              break;
-            }
-          }
-        });
-  }
+        this.diaryService.createEntry(this.diaryId, entry)
+            .subscribe(
+                (data) => {
+                    console.log(data);
+                    this.currentEntryId = data;
+                    this.entryCanBeSaved = true;
+                    this.editorContent = entry.content;
+                    this.refreshEntries();
+                },
+                error => {
+                    switch (error.status) {
+                        case -1: {
+                            //this.openSnackBar("Something went wrong while trying to register anxiety type", "Try again");
+                            break;
+                        }
+                    }
+                });
+    }
 
-  setEditorContent(entry: Entry) {
-    this.editorHidden = false;
-    this.editorContent = entry.content;
-  }
+    setEditorContent(entry: Entry) {
+        this.editorHidden = false;
+        this.currentEntryId = entry.id;
+        this.editorContent = entry.content;
+    }
 
-  generatePreview(content: string, maxLengthPerLine: number): string {
-    const lines = content.match(/<h1>.*?<\/h1>|<h2>.*?<\/h2>|<h3>.*?<\/h3>|<p>.*?<\/p>|<br>/g) || []
+    generatePreview(content: string, maxLengthPerLine: number): string {
+        const lines = content.match(/<h1>.*?<\/h1>|<h2>.*?<\/h2>|<h3>.*?<\/h3>|<p>.*?<\/p>|<br>/g) || []
 
-    const truncatedLines = lines
-      .slice(0, 3)
-      .map((line) => (line.length > maxLengthPerLine ? line.substring(0, maxLengthPerLine) + '...' : line));
+        const truncatedLines = lines
+            .slice(0, 3)
+            .map((line) => (line.length > maxLengthPerLine ? line.substring(0, maxLengthPerLine) + '...' : line));
 
-    return truncatedLines.join('\n');
-  }
+        return truncatedLines.join('\n');
+    }
 
+    refreshEntries() {
+        this.diaryService.getEntriesByDiary(this.diaryId)
+            .subscribe(
+                (data) => {
+                    this.entries = data;
+                },
+                error => {
+                    console.log(error.status);
+                });
+    }
 
-  closeEditor() {
-    this.editorHidden = true;
-  }
+    closeEditor() {
+        this.editorHidden = true;
+    }
 }
--- src/app/patient/diary/service/diary.service.ts
+++ src/app/patient/diary/service/diary.service.ts
@@ -4,31 +4,38 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {AuthService} from "@core";
 import {Diary} from "../model/diary";
+import {Entry} from "../model/entry";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 
 export class DiaryService {
-  private readonly baseUrl = API_URL;
-  private readonly header;
+    private readonly baseUrl = API_URL;
+    private readonly header;
 
-  constructor(
-    private readonly http: HttpClient,
-    private readonly authService: AuthService) {
-    this.header = new HttpHeaders().set("Authorization", 'Bearer ' + this.authService.currentUserValue.token);
-  }
+    constructor(
+        private readonly http: HttpClient,
+        private readonly authService: AuthService) {
+        this.header = new HttpHeaders().set("Authorization", 'Bearer ' + this.authService.currentUserValue.token);
+    }
 
-  getDiaryByUser(): Observable<Diary> {
-    const userId = this.authService.currentUserValue.id;
-    const url = `${this.baseUrl}/getDiary/${userId}`;
+    getDiaryByUser(): Observable<Diary> {
+        const userId = this.authService.currentUserValue.id;
+        const url = `${this.baseUrl}/getDiary/${userId}`;
 
-    return this.http.get<Diary>(url, {headers: this.header});
-  }
+        return this.http.get<Diary>(url, {headers: this.header});
+    }
 
-  createEntry(diaryId: number): Observable<number> {
-    const url = `${this.baseUrl}/addEntry/19/entry`;
+    getEntriesByDiary(diaryId: number): Observable<Entry[]> {
+        const url = `${this.baseUrl}/entries/${diaryId}`;
 
-    return this.http.post<number>(url, {headers: this.header});
-  }
+        return this.http.get<Entry[]>(url, {headers: this.header});
+    }
+
+    createEntry(diaryId: number, entry: Entry): Observable<number> {
+        const url = `${this.baseUrl}/addEntry/19/entry`;
+
+        return this.http.post<number>(url, entry, {headers: this.header});
+    }
 }

3d20dc4b64f34b2da6ad31b351c9f824b55d1a2d
Fabricio Salgado <fabricio@5e.cr>
2023-11-22 18:56:49 -0600

    translate contacts

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -5,13 +5,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
-
 import { PublicRoutingModule } from './public-routing.module';
 import { LandingComponent } from './landing/landing.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
 import { FooterComponent } from './public-layout/footer/footer.component';
 import { HeaderComponent } from './public-layout/header/header.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -29,6 +29,7 @@ import { HeaderComponent } from './public-layout/header/header.component';
     CarouselModule,
     MatIconModule,
     PublicRoutingModule,
+    TranslateModule
   ],
   exports: [
     HeaderComponent,

b87c357ad8dd3eeb726e4387a2e0942718856306
Jocselyn Aguilar <ajocseline@gmail.com>
2023-11-24 00:25:26 -0600

    spinner

--- src/app/patient/patient.module.ts
+++ src/app/patient/patient.module.ts
@@ -37,6 +37,7 @@ import {SpinnerModule} from "../components/components.module";
         SharedModule,
         TranslateModule,
         CKEditorModule,
+        SpinnerModule,
     ],
 })
 export class PatientModule {

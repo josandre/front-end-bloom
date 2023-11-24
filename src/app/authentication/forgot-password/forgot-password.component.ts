65a4832392873c50547b5d8e7bd9eb7ab9d060f1
esequeiras <esequeiras@ucenfotec.ac.cr>
2023-11-24 01:49:45 -0600

    forgot paswword ts, en y es json,

--- src/app/authentication/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password.component.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/forms';
 import { AuthService } from '@core'
 import { MatSnackBar } from '@angular/material/snack-bar'
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
@@ -13,14 +14,18 @@ import { MatSnackBar } from '@angular/material/snack-bar'
 })
 export class ForgotPasswordComponent implements OnInit {
   authForm: FormGroup;
-
+  currentLang: string;
   constructor(
     private formBuilder: FormBuilder,
     private readonly snackBar: MatSnackBar,
-    private readonly authService: AuthService
-  ) {}
+    private readonly authService: AuthService,
+    private translate: TranslateService
+  ) {
+    this.currentLang = 'en';
+  }
 
   ngOnInit() {
+    this.translate.use(this.currentLang);
     this.authForm = this.formBuilder.group({
       email: [
         '',
@@ -39,11 +44,25 @@ export class ForgotPasswordComponent implements OnInit {
     this.authService.changePassword(userEmail)
       .subscribe({
         next: () => {
-          this.snackBar.open("All set! We sent an email with your new details.", 'Done', {verticalPosition: 'top', horizontalPosition: 'end'})
+          this.openSnackBar('SNACKBAR_FORGOT_PASSWORD.SUCCESS', 'SNACKBAR_FORGOT_PASSWORD.DONE')
         },
         error: () => {
-          this.snackBar.open("There was an issue resetting your password. Please contact support-bloomapp@gmail.com for more information.", 'Done', {verticalPosition: 'top', horizontalPosition: 'end'})
+          this.openSnackBar("SNACKBAR_FORGOT_PASSWORD.ERROR", 'SNACKBAR_FORGOT_PASSWORD.DONE')
         }
       })
   }
+  changeLanguage(lang: string): void {
+    this.currentLang = lang;
+    this.translate.use(lang);
+  }
+
+  isLanguageActive(lang: string): boolean {
+    return this.currentLang === lang;
+  }
+  openSnackBar(message: string, action: string) {
+    this.translate.get([message,action]).subscribe((translations: any) => {
+    this.snackBar.open(translations[message], translations[action], { verticalPosition: 'top', horizontalPosition: 'end',duration: 4000 })
+    });
+  }
+  
 }

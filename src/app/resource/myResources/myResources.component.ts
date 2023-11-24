711bb4f23fcc84f5e15abbb3b4f6fbad3f1b2940
Francisco Tovar <jtovara@ucenfotec.ac.cr>
2023-11-23 23:50:55 -0600

    midnight train

--- src/app/resource/myResources/myResources.component.ts
+++ src/app/resource/myResources/myResources.component.ts
@@ -2,42 +2,39 @@ import {Component, OnInit} from '@angular/core';
 import {ResourceService} from "../services/Resource.Service";
 import {Resource} from "../models/Resource";
 import { MatSnackBar } from '@angular/material/snack-bar';
-import {Task} from "../models/Task";
 import {TranslateService} from "@ngx-translate/core";
 @Component({
   selector: 'app-inbox',
   templateUrl: './myResources.component.html',
   styleUrls: ['./myResources.component.scss'],
 })
-export class MyResourcesComponent implements OnInit{
+export class MyResourcesComponent implements OnInit {
 
-  message: string = 'MENUITEMS.RESOURCES.MESSAGE'
+  message: string = 'MENUITEMS.RESOURCES.MESSAGE';
 
   constructor(private readonly resourceService: ResourceService,
               private snackBar: MatSnackBar,
-              private readonly translate:TranslateService) {
-    this.selectedResourceIds=[];
-    }
+              private readonly translate: TranslateService) {
+    this.selectedResourceIds = [];
+  }
 
   resourcesList: Resource[];
   originalResourcesList: Resource[];
   selectedResourceIds: number[];
-
   flag: boolean = false;
 
-  ngOnInit(){
+  ngOnInit() {
     this.flag = true;
     this.resourceService.getResourceList().subscribe(
-      resources =>{
+      resources => {
         this.resourcesList = resources;
-        this.originalResourcesList=[...resources]
+        this.originalResourcesList = [...resources]
         this.flag = false;
       }
-
     )
   }
 
-  sessionResource(id: number){
+  sessionResource(id: number) {
     sessionStorage.setItem('resourseId', id.toString());
   }
 
@@ -68,49 +65,47 @@ export class MyResourcesComponent implements OnInit{
 
     }
   }
-  deleteResource(){
+
+  deleteResource() {
     this.resourceService.deleteResourse(this.selectedResourceIds).subscribe((res) => {
       switch (res) {
-        case 200:{
+        case 200: {
           this.openSnackBar(this.translate.instant('MENUITEMS.RESOURCESNACK.RESDEL'), "Close");
           // Usar filter para crear una nueva lista que excluya los objetos con los IDs a eliminar
           this.resourcesList = this.resourcesList.filter(resource => !this.selectedResourceIds.includes(resource.id));
-          this.originalResourcesList=[...this.resourcesList]
+          this.originalResourcesList = [...this.resourcesList]
           this.selectedResourceIds = [];
           // Restablecer la tabla para mostrar todos los datos
           break;
         }
       }
     }, error => {
-        this.openSnackBar(this.translate.instant('MENUITEMS.RESOURCESNACK.ERROR'), "Close" );
+      this.openSnackBar(this.translate.instant('MENUITEMS.RESOURCESNACK.ERROR'), "Close");
 
-        })
+    })
   }
 
-  deleteThisResource(id: number){
+  deleteThisResource(id: number) {
     this.selectedResourceIds = [id];
     this.resourceService.deleteResourse(this.selectedResourceIds).subscribe((res) => {
       switch (res) {
-        case 200:{
+        case 200: {
           this.openSnackBar(this.translate.instant('MENUITEMS.RESOURCESNACK.RESDEL'), "Close");
           this.resourcesList = this.resourcesList.filter(resource => !this.selectedResourceIds.includes(resource.id));
-          this.originalResourcesList=[...this.resourcesList]
+          this.originalResourcesList = [...this.resourcesList]
           this.selectedResourceIds = [];
 
           break;
         }
       }
     }, error => {
-      this.openSnackBar(this.translate.instant('MENUITEMS.RESOURCESNACK.ERROR'), "Close" );
+      this.openSnackBar(this.translate.instant('MENUITEMS.RESOURCESNACK.ERROR'), "Close");
 
     })
   }
 
-  openSnackBar(message: string, action: string){
+  openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {verticalPosition: 'top', horizontalPosition: 'center', duration: 3000})
   }
 
-  protected readonly Task = Task;
-
-  protected readonly indexedDB = indexedDB;
 }
